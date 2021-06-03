19a92b9d2c5078ef202263a92647326de375686c
Hannes Fremann <hg@automatx.de>
2021-06-03 20:41:48 +0200

    Access database

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-require('colors');
-console.log('smashing node'.rainbow);
-//console.log('Test');
+const { Client } = require('pg')
+const connectionString = "postgresql://hg:12Feuer3@localhost:5432/hg"
+
+select(connectionString);
+console.log("Test");
+
+
+select = async (cs) => {
+    const client = new Client({
+	connectionString: connectionString
+    })
+    
+    client.connect()
+    const res = await client.query('select * from hg.test;', (err, res) => {
+	console.log(res.rows[0].name)
+	client.end()
+    })
+}
+
+
+    
